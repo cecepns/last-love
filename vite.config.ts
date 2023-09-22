3932847e08a59f800361d2887d73b5093af7dbac
Cecep Nandang <cecep@kima.one>
2023-09-22 20:24:18 +0700

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'last-love.vercel.app',
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
