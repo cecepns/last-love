215b9f598ad0b772445ff272da6b56377ba467df
Pasquale Palena <pasquale.palena.lastlove@gmail.com>
2023-09-26 07:30:22 +0700

    fix total questions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,28 @@ import { StatisticsCard } from '@/components/organisms';
 import { ENV } from '@/utils';
 import { useData } from '@/utils';
 import { useMemo } from 'react';
-import { UserResponse } from '@/type';
+import { QuestionResponse, UserResponse } from '@/type';
 
 export const Home: React.FC = () => {
   const { data } = useData<UserResponse>(`${ENV.API_URL}/v1/users/`);
+  const { data: dataQuestion } = useData<QuestionResponse>(`${ENV.API_URL}/v1/questions`);
   
   const totalUsers = useMemo(()=> data?.totalUsers ?? 0, [data?.totalUsers]);
+  const totalQuestion = useMemo(()=> dataQuestion?.totalQuestions ?? 0, [dataQuestion?.totalQuestions]);
 
   return (
     <div className="mt-12">
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
         <StatisticsCard
-          title="User"
+          title="Total Users"
           icon="user"
           value={totalUsers}
+          color="green"
+        />
+        <StatisticsCard
+          title="Total Questions"
+          icon="question"
+          value={totalQuestion}
           color="green"
         />
       </div>
