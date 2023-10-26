16f6e66eca5da3a85cab974812ea1e075450e77a
Pasquale Palena <pasquale.palena.lastlove@gmail.com>
2023-10-26 08:05:55 +0700

    fix count, handle login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -33,7 +33,7 @@ export const Login: React.FC = () => {
 
         const { user } = userCredential;
 
-        if(user.email === 'admin.lastlove@gmail.com') {
+        if(user.email !== 'admin.lastlove@gmail.com') {
           return alert('you don\'t have access');
         }
 
