1e4374d3345e699b78accbc608ca76e62d6de1cd
Cecep Nandang <cecep@kima.one>
2024-01-29 22:12:08 +0700

    fix: download csv

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { CSVLink } from 'react-csv';
+import CsvDownloader from 'react-csv-downloader';
 import moment from 'moment';
 
 import { ENV } from '@/utils/env';
@@ -10,13 +10,16 @@ import { User, UserResponse } from '@/type';
 import { Button, Icon, Typography } from '@/components/atoms';
 import { Table } from '@/components/molecules';
 
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/config';
+
 export const Users: React.FC = () => {
+  const [loadingCsv, setLoadingCsv] = useState<boolean>(false);
   const [page, setPage] = useState(1);
   const navigate = useNavigate();
-  const { data, loading } = useData<UserResponse>(`${ENV.API_URL}/v1/users?page=${page}&limit=10`);
-  const { data: dataAllUsers, loading: loadingUsersDownload } = useData<UserResponse>(`${ENV.API_URL}/v1/users?page=${page}&limit=10000`);
 
-  const allUsers = useMemo(()=> dataAllUsers?.users.filter((v) => v.email !== 'admin.lastlove@gmail.com') ?? [], [dataAllUsers?.users]);
+  const { data, loading } = useData<UserResponse>(`${ENV.API_URL}/v1/users?page=${page}&limit=10`);
+
   const users = useMemo(()=> data?.users.filter((v) => v.email !== 'admin.lastlove@gmail.com') ?? [], [data?.users]);
   const totalPages = useMemo(()=> data?.totalPages ?? 0, [data?.totalPages]);
 
@@ -24,13 +27,10 @@ export const Users: React.FC = () => {
     navigate('/dashboard/users/' + value);
   }, [navigate]);
 
-  console.log(users);
-
-  const headersUsers = useMemo(() => [
-    { label: 'Email', key: 'email' },
-    { label: 'Name', key: 'name' },
-    { label: 'Ispaid', key: 'isPaid' },
-    { label: 'IsVerified', key: 'isVerified' }
+  const columnDataUsers = useMemo(() => [
+    { displayName: 'Email', id: 'email' },
+    { displayName: 'Name', id: 'name' },
+    { displayName: 'Questions & Answeres', id: 'questions' }
   ], []);
 
   const columnsCarrier = useMemo(() => [
@@ -68,18 +68,6 @@ export const Users: React.FC = () => {
     setPage(val);
   }, []);
 
-  const getAllUsers = useCallback((_: unknown, done:any) => {
-    try {
-      if (!loadingUsersDownload) {
-        return done(true);
-      }
-      done(false);
-    } catch (error) {
-      console.error(error);
-      done(false); 
-    }
-  }, [loadingUsersDownload]);
-
   const tableConfig = {
     columns: columnsCarrier,
     data: users,
@@ -89,25 +77,77 @@ export const Users: React.FC = () => {
     setCurrentPage: (val: number) => handleChangePage(val)
   };
 
+  const fetchDataFromFirestore = async () => {
+    try {
+      setLoadingCsv(true);
+      const collection1 = await getDocs(collection(db, 'Users'));
+      const collection2 = await getDocs(collection(db, 'UserInfo'));
+
+      console.log('collection 1', collection1);
+      console.log('collection 2', collection2);
+  
+      const newData1:any = collection1.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const newData2:any = collection2.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  
+      const mergedData = newData1.map((data1:any) => {
+        const matchingData2 = newData2.find((data2:any) => data2.id === data1.uid);
+  
+        if (matchingData2) {
+          const pertanyaanArray = matchingData2.answeres ? Object.entries(matchingData2.answeres).map(([pertanyaanKey, jawaban]) => ({ pertanyaan: pertanyaanKey, jawaban })) : [];
+          return { ...data1, pertanyaan: pertanyaanArray };
+        }
+  
+        return data1;
+      });
+  
+      const csvData = mergedData.map(({ email, name, pertanyaan }:any) => {
+        const questionsString = Array.isArray(pertanyaan)
+          ? pertanyaan.map(({ pertanyaan, jawaban }, index) => `${index + 1}.${pertanyaan} = ${jawaban}`).join(', ')
+          : '';
+        return { email, name, questions: questionsString };
+      });
+  
+      setLoadingCsv(false);
+      return csvData;
+    } catch (error) {
+      setLoadingCsv(false);
+      console.error('Error fetching collections:', error);
+      throw error;
+    }
+  };
+  
+  const asyncFnComputeDate = async () => {
+    try {
+      const csvData = await fetchDataFromFirestore();
+      console.log('CSV', csvData);
+      return csvData;
+    } catch (error) {
+      alert(error);
+      // Handle error appropriately
+    }
+  };
+
   return (
     <div className="mt-12">
       <div>
         <Typography>
           Data Users
         </Typography>
-        
+
         <div className="relative">
-          
-          <CSVLink className="absolute z-10 top-[30px] z-index-1 bg-primary hover:bg-primary-700 text-white px-2 py-2 justify-center middle none font-sans font-bold center transition-all  text-xs rounded-lg  flex items-center gap-4 capitalize w-[130px]"
-            asyncOnClick={true}
-            onClick={getAllUsers}
+
+          <CsvDownloader
+            className="absolute z-10 top-[30px] z-index-1"
+            disabled={loadingCsv}
+            datas={asyncFnComputeDate}
+            columns={columnDataUsers}
             filename={`users-data-${moment().format('DD-MM-YYYY HH:mm')}`}
-            data={allUsers}
-            headers={headersUsers}
-          >
-            {loadingUsersDownload ? 'loading...' : 'Export data' }
-            <Icon name="file-csv"/>
-          </CSVLink>
+          > 
+            <Button disabled={loadingCsv}>
+              {loadingCsv ? 'loading...' : 'Export data' }
+              <Icon name="file-csv"/>
+            </Button>
+          </CsvDownloader>
           
           <Table {...tableConfig}/>
         </div>
