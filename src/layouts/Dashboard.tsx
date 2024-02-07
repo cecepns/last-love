9fb42df42724b7b13748e501a30169b086209de8
Cecep Nandang <cecep@kima.one>
2024-02-07 23:55:24 +0700

    validate dashboard

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,9 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import { DashboardNavbar, Sidenav } from '@/components/molecules';
 import routes, { Layout, PageRoutes } from '@/routes';
+import { useSession } from '@/hooks';
+import { useEffect, useState } from 'react';
 
 export const Dashboard = ()=> {
+  const [isInit, setIsInit] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const [, setSesstion] = useSession();
+
+  useEffect(() => {
+    const initFn = async () => {
+      const sessionToken = localStorage.getItem('sessionToken');
+
+      if(sessionToken) {
+        const sessionAccount = localStorage.getItem('sessionAccount');
+        if(sessionAccount) {
+          setSesstion(JSON.parse(sessionAccount));
+          navigate('dashboard/home');
+        }
+      } else if (!sessionToken) {
+        localStorage.removeItem('sessionToken');
+        localStorage.removeItem('sessionAccount');
+        navigate('auth/signin');
+      }
+    };
+    if (!isInit) {
+      setIsInit(true);
+      initFn();
+    }
+  }, [isInit, navigate, setSesstion]);
+  
   return (
     <div className="min-h-screen bg-gray-100/[.5]">
       <Sidenav
